e7ee8a8976026b7d385e27831ce5ecb273f1f8df
Ibrokhim <132742784+ibrodevs@users.noreply.github.com>
2026-08-09 12:11:12 +0600

    Remove premium feature guards

--- lib/features/subscription/premium_guard.dart
+++ lib/features/subscription/premium_guard.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../core/subscriptions/subscription_service.dart';
-import '../../l10n/app_localizations.dart';
-import 'premium_onboarding_screen.dart';
-
+/// Legacy feature identifiers kept so existing screens do not need invasive
+/// changes. The app is fully free, therefore every feature is always allowed.
 enum PremiumFeature {
   additionalChildren,
   liveMap,
@@ -18,76 +15,16 @@ enum PremiumFeature {
   generic,
 }
 
-extension PremiumFeatureCopy on PremiumFeature {
-  String titleFor(BuildContext context) {
-    final t = S.of(context);
-    return switch (this) {
-      PremiumFeature.additionalChildren => t.premiumTitleAdditionalChildren,
-      PremiumFeature.liveMap => t.premiumTitleLiveMap,
-      PremiumFeature.movementHistory => t.premiumTitleMovementHistory,
-      PremiumFeature.audioMonitoring => t.premiumTitleAudioMonitoring,
-      PremiumFeature.screenTime => t.premiumTitleScreenTime,
-      PremiumFeature.appStats => t.premiumTitleAppStats,
-      PremiumFeature.achievements => t.premiumTitleAchievements,
-      PremiumFeature.loudAlarm => t.premiumTitleLoudAlarm,
-      PremiumFeature.fullMenu => t.premiumTitleFullMenu,
-      PremiumFeature.generic => t.premiumTitleGeneric,
-    };
-  }
-
-  String subtitleFor(BuildContext context) {
-    final t = S.of(context);
-    return switch (this) {
-      PremiumFeature.additionalChildren => t.premiumSubtitleAdditionalChildren,
-      PremiumFeature.liveMap => t.premiumSubtitleLiveMap,
-      PremiumFeature.movementHistory => t.premiumSubtitleMovementHistory,
-      PremiumFeature.audioMonitoring => t.premiumSubtitleAudioMonitoring,
-      PremiumFeature.screenTime => t.premiumSubtitleScreenTime,
-      PremiumFeature.appStats => t.premiumSubtitleAppStats,
-      PremiumFeature.achievements => t.premiumSubtitleAchievements,
-      PremiumFeature.loudAlarm => t.premiumSubtitleLoudAlarm,
-      PremiumFeature.fullMenu => t.premiumSubtitleFullMenu,
-      PremiumFeature.generic => t.premiumSubtitleGeneric,
-    };
-  }
-}
-
 Future<bool> requirePremium(
   BuildContext context, {
   PremiumFeature feature = PremiumFeature.generic,
 }) async {
-  final container = ProviderScope.containerOf(context, listen: false);
-  final state = container.read(subscriptionServiceProvider);
-  if (state.isPremium) return true;
-
-  final result = await Navigator.of(context).push<bool>(
-    MaterialPageRoute(
-      fullscreenDialog: true,
-      builder: (_) => PremiumOnboardingScreen(feature: feature),
-    ),
-  );
-
-  if (result == true) {
-    return true;
-  }
-
-  return container.read(subscriptionServiceProvider).isPremium;
+  return true;
 }
 
 Future<bool> requirePremiumForAdditionalChild(
   BuildContext context, {
   required int currentChildrenCount,
 }) async {
-  final container = ProviderScope.containerOf(context, listen: false);
-  final state = container.read(subscriptionServiceProvider);
-  if (canAccessMultipleChildren(
-    isPremium: state.isPremium,
-    currentChildrenCount: currentChildrenCount,
-  )) {
-    return true;
-  }
-  return requirePremium(
-    context,
-    feature: PremiumFeature.additionalChildren,
-  );
+  return true;
 }
