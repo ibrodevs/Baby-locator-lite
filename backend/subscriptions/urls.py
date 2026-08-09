bb24ed03a4a24bc70f13a6863433d18785379d7d
Ibrokhim <132742784+ibrodevs@users.noreply.github.com>
2026-08-09 12:52:05 +0600

    Expose Lite token exchange route

--- backend/subscriptions/urls.py
+++ backend/subscriptions/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import RevenueCatWebhookView
+from .views import LiteAccessTokenView, RevenueCatWebhookView
 
 urlpatterns = [
+    path("lite-token/", LiteAccessTokenView.as_view()),
     path("webhook/", RevenueCatWebhookView.as_view()),
 ]
